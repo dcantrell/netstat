bd923f798ebeb2acbc380b75c3396b140d76ed4b
David Cantrell <david.l.cantrell@gmail.com>
2016-09-19 23:06:23 -0400

    Conditionalize some macros for IPv6 and Bluetooth.

--- src/pathnames.h
+++ src/pathnames.h
@@ -11,22 +11,14 @@
 
 /* pathnames of the procfs files used by NET. */
 #define _PATH_PROCNET_IGMP		"/proc/net/igmp"
-#define _PATH_PROCNET_IGMP6		"/proc/net/igmp6"
 #define _PATH_PROCNET_TCP		"/proc/net/tcp"
-#define _PATH_PROCNET_TCP6		"/proc/net/tcp6"
 #define _PATH_PROCNET_UDP		"/proc/net/udp"
-#define _PATH_PROCNET_UDP6		"/proc/net/udp6"
 #define _PATH_PROCNET_UDPLITE           "/proc/net/udplite"
-#define _PATH_PROCNET_UDPLITE6          "/proc/net/udplite6"
 #define _PATH_PROCNET_SCTPEPTS		"/proc/net/sctp/eps"
-#define _PATH_PROCNET_SCTP6EPTS		"/proc/net/sctp6/eps"
 #define _PATH_PROCNET_SCTPASSOCS	"/proc/net/sctp/assocs"
-#define _PATH_PROCNET_SCTP6ASSOCS	"/proc/net/sctp6/assocs"
 #define _PATH_PROCNET_RAW		"/proc/net/raw"
-#define _PATH_PROCNET_RAW6		"/proc/net/raw6"
 #define _PATH_PROCNET_UNIX		"/proc/net/unix"
 #define _PATH_PROCNET_ROUTE		"/proc/net/route"
-#define _PATH_PROCNET_ROUTE6		"/proc/net/ipv6_route"
 #define _PATH_PROCNET_RTCACHE		"/proc/net/rt_cache"
 #define _PATH_PROCNET_AX25_ROUTE	"/proc/net/ax25_route"
 #define _PATH_PROCNET_NR		"/proc/net/nr"
@@ -44,7 +36,19 @@
 #define _PATH_PROCNET_IP_ACC		"/proc/net/ip_acct"
 #define _PATH_PROCNET_IP_MASQ		"/proc/net/ip_masquerade"
 #define _PATH_PROCNET_NDISC		"/proc/net/ndisc"
+
+#ifdef HAVE_AFINET6
+#define _PATH_PROCNET_IGMP6		"/proc/net/igmp6"
+#define _PATH_PROCNET_TCP6		"/proc/net/tcp6"
+#define _PATH_PROCNET_UDP6		"/proc/net/udp6"
+#define _PATH_PROCNET_UDPLITE6          "/proc/net/udplite6"
 #define _PATH_PROCNET_IFINET6		"/proc/net/if_inet6"
+#define _PATH_PROCNET_SCTP6EPTS		"/proc/net/sctp6/eps"
+#define _PATH_PROCNET_SCTP6ASSOCS	"/proc/net/sctp6/assocs"
+#define _PATH_PROCNET_RAW6		"/proc/net/raw6"
+#define _PATH_PROCNET_ROUTE6		"/proc/net/ipv6_route"
+#endif
+
 #define _PATH_PROCNET_DEV		"/proc/net/dev"
 #define _PATH_PROCNET_RARP		"/proc/net/rarp"
 #define _PATH_ETHERS			"/etc/ethers"
@@ -53,8 +57,11 @@
 #define _PATH_PROCNET_X25_ROUTE		"/proc/net/x25/route"
 #define _PATH_PROCNET_DEV_MCAST		"/proc/net/dev_mcast"
 #define _PATH_PROCNET_ATALK_ROUTE	"/proc/net/atalk_route"
+
+#ifdef HAVE_AFBLUETOOTH
 #define _PATH_SYS_BLUETOOTH_L2CAP	"/sys/kernel/debug/bluetooth/l2cap"
 #define _PATH_SYS_BLUETOOTH_RFCOMM	"/sys/kernel/debug/bluetooth/rfcomm"
+#endif
 
 /* pathname for the netlink device */
 #define _PATH_DEV_ROUTE	"/dev/route"
